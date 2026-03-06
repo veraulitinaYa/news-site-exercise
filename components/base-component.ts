d6853ffb0daf35525c2c505c19c8a48fcb753f5f
veraulitinaYa <veraulitina@yandex.ru>
2026-03-06 16:45:01 +0300

    feat: add html structure and style files

---
+++ components/base-component.ts
@@ -0,0 +1,12 @@
+export abstract class BaseComponent<T> {
+    constructor(protected readonly container: HTMLElement) {
+
+    }
+    public get element(): HTMLElement {
+        return this.container;
+    }
+    render(data?: Partial<T>): HTMLElement {
+        Object.assign(this as object, data ?? {});
+        return this.container;
+    }
+}
